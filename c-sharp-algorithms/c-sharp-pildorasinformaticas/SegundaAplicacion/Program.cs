aad032ca67fd4cb8f98efe45c9e2e58faba4b688
marcoquind <marco.ortega@quind.io>
2020-01-28 22:11:34 -0500

    28 january 2020

---
+++ c-sharp-algorithms/c-sharp-pildorasinformaticas/SegundaAplicacion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SegundaAplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
